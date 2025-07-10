41d35cd902bfe6c6278befc748a2aee7219797b7
Michael Jardine <mikej1798@gmail.com>
2025-07-10 16:35:05 -0600

    added new preliminary maps.  Bottom truck exit will warp to new map for testing.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,15 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/HarmonyBeach/scripts.inc"
+
+	.include "data/maps/MelodyForest/scripts.inc"
+
+	.include "data/maps/HarmonyCaveExt/scripts.inc"
+
+	.include "data/maps/HarmonySandbar/scripts.inc"
+
+	.include "data/maps/HarmonyVillage/scripts.inc"
+
+	.include "data/maps/HarmonyBluff/scripts.inc"
