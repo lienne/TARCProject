bde6c74e8a24bb14367212633a17e081fda57843
Belle <lianasevero3@gmail.com>
2025-07-16 22:23:23 -0400

    Skipped intro movie and birch speech, set player to always male, spawned player in the new map

--- src/expansion_intro.c
+++ src/expansion_intro.c
@@ -13,6 +13,7 @@
 #include "expansion_intro.h"
 #include "constants/rgb.h"
 #include "constants/songs.h"
+#include "title_screen.h"
 
 #if EXPANSION_INTRO == TRUE
 
@@ -278,8 +279,9 @@ void Task_HandleExpansionIntro(u8 taskId)
             ResetSpriteData();
             FreeAllSpritePalettes();
             DestroyTask(taskId);
-            CreateTask(Task_Scene1_Load, 0);
-            SetMainCallback2(MainCB2_Intro);
+            // CreateTask(Task_Scene1_Load, 0);
+            // SetMainCallback2(MainCB2_Intro);
+            SetMainCallback2(CB2_InitTitleScreen);
         }
         break;
     }
